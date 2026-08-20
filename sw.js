f8d3aa0202e7e223fce3590fda4c82a78397664e
czaja393 <aiproducter@gmail.com>
2026-08-20 18:34:40 +0200

    Ichimoku we wlasnym folderze, detektor SCISK, kanaly wysuniete w przyszlosc, coiny w nowych kartach, filtr otwartych pozycji

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C = "chmura-20.08-1801";
+const C = "chmura-20.08-1831";
 self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", (e) => e.waitUntil((async () => {
   for (const k of await caches.keys()) if (k !== C) await caches.delete(k);
