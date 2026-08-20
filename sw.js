dd04283a8c791f0219bee5a52b9034e72697e458
czaja393 <aiproducter@gmail.com>
2026-08-20 08:48:37 +0200

    Kanaly z 1D i 4H widoczne na wszystkich interwalach + szersze szukanie i tolerancja knotow

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C = "chmura-20.08-0837";
+const C = "chmura-20.08-0847";
 self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", (e) => e.waitUntil((async () => {
   for (const k of await caches.keys()) if (k !== C) await caches.delete(k);
