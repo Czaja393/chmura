17f32298620308ac0750c0ba86c6f7e91d85a510
czaja393 <aiproducter@gmail.com>
2026-08-20 18:42:57 +0200

    Neutralnie czarna paleta zamiast cieplych szarosci, cienszy wypelniacz kanalow

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C = "chmura-20.08-1831";
+const C = "chmura-20.08-1842";
 self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", (e) => e.waitUntil((async () => {
   for (const k of await caches.keys()) if (k !== C) await caches.delete(k);
